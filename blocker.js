c5dfc6aea9ff702143e2d2c26040f12c1e2c272c
Aira Catherine Gutierrez <50927914+ac-gutierrez@users.noreply.github.com>
2023-03-25 19:25:32 +0800

    Add files via upload

--- blocker.js
+++ blocker.js
@@ -2,31 +2,57 @@
 
 const defineCss = () =>{
     return `<style>
-    body {
-        margin: 0;
-        padding: 0;
-        height: 100vh;
+    body{
+        background-color: yellow
+      }
+      
+      .rotating-text-wrapper {
+        height: 90vh;
         display: flex;
+        flex-direction: column;
+        align-items: center;
         justify-content: center;
-        align-items : center;
-    }
-    img {
-        max-width: 1000px;
-        height: auto;
-
-    }
-</style>`
+        text-align: center;
+      }
+      .rotating-text-wrapper h2 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 2.5em;
+        margin: 0;
+        padding: 0.3em;
+        color: #fff;
+        box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
+        animation-duration: 6s;
+        animation-iteration-count: infinite;
+        opacity: 0;
+      }
+      .rotating-text-wrapper h2:nth-child(1) {
+        background-color: BLACK;
+        animation-name: rotating-text-1;
+      }
+      @keyframes rotating-text-1 {
+        0%, 20%, 40%, 60%, 80%, 100% {
+          transform: scale(1, 1);
+          opacity: 1;
+        }
+        10%, 30%, 50%, 70%, 90% {
+          transform: scale(0.92, 0.95);
+          opacity: 1;
+        }
+      }
+      </style>
+      `
 }
 
 const defineHtml = () => {
-    return `<html>
-        <div class = "content">
-        <img src = "https://media.tenor.com/SREYeZa6ySMAAAAC/back-to-work.gif"
-    </html>`
+        return `<html>
+        <div class="rotating-text-wrapper">
+        <h2>WORK LIKE RIHANNA</h2>
+        </div>
+        </html>`
 }
 
 
-const forbid = ["www.facebook.com", "www.twitter.com", "www.linkedin.com"];
+const forbid = ["www.facebook.com", "twitter.com", "www.linkedin.com"];
 //const set = new Set(forbid);
 
 if(forbid.includes(window.location.hostname)){
